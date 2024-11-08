09f6607932cb323cbb5873b436490c4c886cb804
seo003 <sy030212@naver.com>
2024-11-08 20:09:48 +0900

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,26 @@
 <template>
-  footer
+  <footer id="footer" role="banner">
+      <div class="footer_inner">
+        &copy; 2024 seo003<br/>
+      </div>
+  </footer>
 </template>
+
+
+<script>
+</script>
+
+
+<style lang="scss">
+
+.footer_inner {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(116, 99, 99, 0.1);
+  backdrop-filter: blur(15px);
+  padding: 1rem;
+  text-align:center;
+}
+
+</style>
